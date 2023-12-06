b58113b8ef594b88bb5c4271911c66d6dce2b7c3
teriTheDesigner <tere1109@stud.kea.dk>
2023-12-06 21:57:43 +0100

    slider fixed

--- app/page.js
+++ app/page.js
@@ -10,12 +10,12 @@ import SponsorsinitFor from "./components/SponsorsinitFor/SponsorsinitFor";
 export default function Home() {
   return (
     <main className={styles.main}>
-      {/* <h1 className={styles.h1}>GAME AUDIO DENMARK</h1>
-      <Slider /> */}
+      {/* <h1 className={styles.h1}>GAME AUDIO DENMARK</h1> */}
 
       <Numbers />
       <MembershipCard />
       <OurSponsors />
+      <Slider />
       <SponsorsinitFor />
       <SponsorAttractCards />
       <SponsorshipCards />
