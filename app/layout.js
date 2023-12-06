138c8c3a16de9b84df1387c76d9b2c64ff436663
teriTheDesigner <tere1109@stud.kea.dk>
2023-12-06 22:27:31 +0100

    fix: margin on body

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={raleway.className}>
+      <body className={`${raleway.className} body`}>
         <Nav />
         {children}
         <Footer />
