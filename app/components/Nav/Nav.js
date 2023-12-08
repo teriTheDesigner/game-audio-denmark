929e1e5c801d8244541322a5138c0a2a30c34bd0
teriTheDesigner <tere1109@stud.kea.dk>
2023-12-08 19:13:23 +0100

    feat: responsive navigation

--- app/components/Nav/Nav.js
+++ app/components/Nav/Nav.js
@@ -1,6 +1,7 @@
 "use client";
-import styles from "./Nav.module.css";
+
 import { useState } from "react";
+import styles from "./Nav.module.css";
 import Link from "next/link";
 
 export default function Nav() {
@@ -9,33 +10,63 @@ export default function Nav() {
   const handleToggleMenu = () => {
     setMenuOpen(!isMenuOpen);
   };
+
   return (
-    <header className={styles.header}>
-      <p>LOGO</p>
-      <nav className={styles.nav}>
-        <Link href="/events" className={styles.a}>
-          EVENTS
-        </Link>
+    <div>
+      <header className={styles.header}>
+        <p>LOGO</p>
 
-        <Link href="/blog" className={styles.a}>
-          BLOG
-        </Link>
+        <nav className={styles.nav}>
+          <Link href="/events" className={styles.a}>
+            EVENTS
+          </Link>
 
-        <Link href="/about" className={styles.a}>
-          ABOUT US
-        </Link>
+          <Link href="/blog" className={styles.a}>
+            BLOG
+          </Link>
 
-        <Link href="/sponsors" className={styles.a}>
-          SPONSORS
-        </Link>
-
-        <Link href="/bylaws" className={styles.a}>
-          BYLAWS
-        </Link>
-      </nav>
-      <button className={styles.burger} onClick={handleToggleMenu}>
-        MENU
-      </button>
-    </header>
+          <Link href="/about" className={styles.a}>
+            ABOUT US
+          </Link>
+          <Link href="/sponsors" className={styles.a}>
+            SPONSORS
+          </Link>
+          <Link href="/bylaws" className={styles.a}>
+            BYLAWS
+          </Link>
+        </nav>
+      </header>
+      <div className={styles.burgermenu}>
+        <div
+          className={`${styles.container} ${isMenuOpen ? styles.menuOpen : ""}`}
+          onClick={handleToggleMenu}
+        >
+          <div className={`${styles.menulogo} ${styles.line1}`}></div>
+          <div className={`${styles.menulogo} ${styles.line2}`}></div>
+          <div className={`${styles.menulogo} ${styles.line3}`}></div>
+        </div>
+        {isMenuOpen && (
+          <div className={`${styles.menu} ${styles.showmenu}`}>
+            <ul>
+              <li>
+                <Link href="/events">EVENTS</Link>
+              </li>
+              <li>
+                <Link href="/blog">BLOG</Link>
+              </li>
+              <li>
+                <Link href="/about">ABOUT US</Link>
+              </li>
+              <li>
+                <Link href="/sponsors">SPONSORS</Link>
+              </li>
+              <li>
+                <Link href="/bylaws">BYLAWS</Link>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
