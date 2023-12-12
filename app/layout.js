c0a39ec82b304b56f5667c950b9d2e73254c93a3
teriTheDesigner <tere1109@stud.kea.dk>
2023-12-12 12:40:03 +0100

    feat: applying second font

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,13 @@
-import { Raleway } from "next/font/google";
+import { Raleway, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import Nav from "@/app/components/Nav/Nav";
 import Footer from "./components/Footer/Footer";
 
 const raleway = Raleway({ subsets: ["latin"] });
+const roboto_mono = Roboto_Mono({
+  subsets: ["latin"],
+  variable: "--font-roboto-mono",
+});
 
 export const metadata = {
   title: "Game Audio Denmark",
@@ -12,7 +16,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={` ${roboto_mono.variable}`}>
       <body className={`${raleway.className} body`}>
         <Nav />
         {children}
